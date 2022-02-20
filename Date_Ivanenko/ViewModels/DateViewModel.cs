b9d4f5d9a037e892628ce507faba78a135ac0ba5
Lera <valeria.ivanenko@icloud.com>
2022-02-20 05:54:31 +0200

    Добавьте файлы проекта.

---
+++ Date_Ivanenko/ViewModels/DateViewModel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Desktop.Date_Ivanenko.Tools;
+
+namespace Desktop.Date_Ivanenko.ViewModels
+{
+    class DateViewModel
+    {
+        private RelayCommand<object> _commitCommand;
+        private DateTime _userBirthDate;
+        private int _age;
+        private string _westernZodiac;
+        private string _chineseZodiac;
+
+        public DateTime UserBirthDate
+        {
+            get { return _userBirthDate; }
+            set { _userBirthDate = value;
+                SetAge();
+                SetZodiac();
+                SetChineseZodeac();
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value;
+            }
+        }
+        public string WesternZodiac
+        {
+            get { return _westernZodiac; }
+            set { _westernZodiac = value; }
+        }
+
+        public string ChineseZodiac
+        {
+            get { return _chineseZodiac; }
+            set { _chineseZodiac = value; }
+        }
+
+        public RelayCommand<object> CommitCommand
+        {
+            get
+            {
+                return _commitCommand ??= new RelayCommand<object>(_ => Commit(), CanExecute);
+            }
+        }
+
+        private void Commit()
+        {
+            if ((UserBirthDate.Month == DateTime.Today.Month) && (UserBirthDate.Day == DateTime.Today.Day))
+            {
+                MessageBox.Show("Happy Birthday!");
+            }
+        }
+
+        private bool CanExecute(object obj)
+        {
+            SetAge();
+            return !(UserBirthDate > DateTime.Today) && !(Age > 135);
+        }
+
+        public void SetAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - UserBirthDate.Year;
+            if (UserBirthDate.Date > today.AddYears(-age)) age--;
+            Age = age;
+        }
+
+        public void SetZodiac()
+        {
+            switch (UserBirthDate.Month)
+            {
+                case 1:
+                    if (UserBirthDate.Day <= 20) WesternZodiac = "Capricorn";
+                    else
+                    { WesternZodiac = "Aquarius"; }
+                    break;
+                case 2:
+                    if (UserBirthDate.Day <= 19)
+                    { WesternZodiac = "Aquarius"; }
+                    else
+                    { WesternZodiac = "Pisces"; }
+                    break;
+                case 3:
+                    if (UserBirthDate.Day <= 20)
+                    { WesternZodiac = "Pisces"; }
+                    else
+                    { WesternZodiac = "Aries"; }
+                    break;
+                case 4:
+                    if (UserBirthDate.Day <= 20)
+                    { WesternZodiac = "Aries"; }
+                    else
+                    { WesternZodiac = "Taurus"; }
+                    break;
+                case 5:
+                    if (UserBirthDate.Day <= 21)
+                    { WesternZodiac = "Taurus"; }
+                    else
+                    { WesternZodiac = "Gemini"; }
+                    break;
+                case 6:
+                    if (UserBirthDate.Day <= 22)
+                    { WesternZodiac = "Gemini"; }
+                    else
+                    { WesternZodiac = "Cancer"; }
+                    break;
+                case 7:
+                    if (UserBirthDate.Day <= 22)
+                    { WesternZodiac = "Cancer"; }
+                    else
+                    { WesternZodiac = "Leo"; }
+                    break;
+                case 8:
+                    if (UserBirthDate.Day <= 23)
+                    { WesternZodiac = "Leo"; }
+                    else
+                    { WesternZodiac = "Virgo"; ; }
+                    break;
+                case 9:
+                    if (UserBirthDate.Day <= 23)
+                    { WesternZodiac = "Virgo"; }
+                    else
+                    { WesternZodiac = "Libra"; }
+                    break;
+                case 10:
+                    if (UserBirthDate.Day <= 23)
+                    { WesternZodiac = "Libra"; }
+                    else
+                    { WesternZodiac = "Scorpio"; ; }
+                    break;
+                case 11:
+                    if (UserBirthDate.Day <= 22)
+                    { WesternZodiac = "Scorpio"; }
+                    else
+                    { WesternZodiac = "Sagittarius"; }
+                    break;
+                case 12:
+                    if (UserBirthDate.Day <= 21)
+                    { WesternZodiac = "Sagittarius"; }
+                    else
+                    { WesternZodiac = "Capricorn"; }
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void SetChineseZodeac()
+        {
+            string[] animals = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
+            string[] elements = { "Wood", "Fire", "Earth", "Metal", "Water" };
+
+
+            int ei = (int)Math.Floor((UserBirthDate.Year - 4.0) % 10 / 2);
+            int ai = (UserBirthDate.Year - 4) % 12;
+
+            _chineseZodiac = elements[ei] + " " + animals[ai];
+        }
+    }
+}
---
+++ Date_Ivanenko/Views/DateView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Desktop.Date_Ivanenko.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop.Date_Ivanenko.Views
+{
+    public partial class DateView : UserControl
+    {
+        private DateViewModel _viewModel;
+        public DateView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel = new DateViewModel();
+        }
+
+        private void DpDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime? date = DpDate.SelectedDate.Value.Date;
+            if (date == null)
+            {
+
+            } else
+            {
+                _viewModel.UserBirthDate = DpDate.SelectedDate.Value.Date;
+                if (!(_viewModel.UserBirthDate > DateTime.Today) && !(_viewModel.Age > 135))
+                {
+                    TbAge.Text = _viewModel.Age.ToString();
+                    TbWestZodiac.Text = _viewModel.WesternZodiac;
+                    TbChineseZodiac.Text = _viewModel.ChineseZodiac;
+                } else
+                {
+                    MessageBox.Show("Inccorect Date!");
+                }
+            }
+
+        }
+    }
+}
